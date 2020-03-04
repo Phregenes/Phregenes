65f90e7a1a03aa5a8caa10a4b2c5d4eba626baec
Pedro Regenes <phregenes@gmail.com>
2020-03-03 22:09:59 -0300

    Feat: "hover main"

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.css';
 
-const Main = () => <h1 className="main">Olá!! sou Pedro Regenes,<br /> desenvolvedor Front-End<br /> em São Paulo.</h1>;
+const Main = () => <div className="main"><h1>Olá!! sou Pedro Regenes,<br /> desenvolvedor Front-End<br /> em São Paulo.</h1></div>;
 
 export default Main;
