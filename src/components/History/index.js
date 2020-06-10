4dc02670b67dd5253163a93564280b42680547ce
Pedro Regenes <phregenes@gmail.com>
2020-06-10 10:03:37 -0300

    test: presentation

--- src/components/History/index.js
+++ src/components/History/index.js
@@ -6,39 +6,33 @@ import Equipe from '../../assets/img/equipe.JPG';
 function History() {
   return (
     <div className="container_card">
-      <div className="card">
-        <div className="imgBx">
-        <h3>Compartilhando conhecimentos</h3>
-          <img src={Cubo}/>
-        </div>
-        <div className="details">
-          <h3>Compartilhando conhecimentos</h3>
-          <p>Durante tres anos seguidos atuei ensinando tecnicas de cubo magico para as novas turmas que entravam participando da comunidade e passando o conhecimento a diante.</p>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="imgBx">
-        <h3>Organização Campeonato</h3>
-          <img src={Cubo}/>
-        </div>
-        <div className="details">
-          <h3>Cubo Mágico</h3>
-          <p>Obtive o privilegio de poder organizar o segundo maior campeonato de cubo magico do brasil com 5 modalidades diferentes e 163 participantes</p>
-        </div>
-      </div>
-    
-      <div className="card">
-        
-        <div className="imgBx">
-        <h3>Campeonato Cubo Mágico</h3>
-          <img src={Equipe}/>
-        </div>
-        <div className="details">
-          <h3>Cubo Mágico</h3>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-        </div>
-      </div>
+      
+      <div className="content">
+                
+                <div class="grid">
+                    <figure class="effect-lily">
+                        <img src="https://tympanus.net/Development/HoverEffectIdeas/img/12.jpg" alt="img12"/>
+                        <figcaption>
+                            <div>
+                                <h2>Nice <span>Lily</span></h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut metus sit amet libero porttitor varius id nec enim. Quisque non purus ex. Ut pulvinar elit diam, at varius lacus maximus vel. Nulla tempor ligula ex. Nullam hendrerit erat vitae viverra fringilla. Etiam risus velit, euismod id eleifend id, mollis sit amet est. Proin vitae metus eu orci malesuada dapibus vel sit amet odio. Donec vel magna diam. Nulla porttitor posuere urna, nec hendrerit tortor. Cras vestibulum diam nibh. Curabitur facilisis urna sed dui bibendum, in mollis tellus varius. Vivamus feugiat sapien nec tellus finibus, eget maximus erat ornare. Morbi at cursus sapien.</p>
+                            </div>
+                            <a href="#">View more</a>
+                        </figcaption>           
+                    </figure>
+                    <figure class="effect-lily">
+                        <img src="https://tympanus.net/Development/HoverEffectIdeas/img/1.jpg" alt="img1"/>
+                        <figcaption>
+                            <div>
+                                <h2>Nice <span>Lily</span></h2>
+                                <p>Lily likes to play with crayons and pencils</p>
+                            </div>
+                            <a href="#">View more</a>
+                        </figcaption>           
+                    </figure>
+                </div>
+                
+            </div>
     
     </div>
         
