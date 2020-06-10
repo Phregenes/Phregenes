9b536f72658f11de2b8c58633ee6a7fbd506a595
Pedro Regenes <phregenes@gmail.com>
2020-06-10 10:41:35 -0300

    Test: efect css

--- src/components/History/index.js
+++ src/components/History/index.js
@@ -5,37 +5,19 @@ import Equipe from '../../assets/img/equipe.JPG';
 
 function History() {
   return (
-    
-      
-      <div className="content">
-                
-                <div class="grid">
-                    <figure class="effect-lily">
-                        <img src="https://tympanus.net/Development/HoverEffectIdeas/img/12.jpg" alt="img12"/>
-                        <figcaption>
-                            <div>
-                                <h2>Nice <span>Lily</span></h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut metus sit amet libero porttitor varius id nec enim. Quisque non purus ex. Ut pulvinar elit diam, at varius lacus maximus vel. Nulla tempor ligula ex. Nullam hendrerit erat vitae viverra fringilla. Etiam risus velit, euismod id eleifend id, mollis sit amet est. Proin vitae metus eu orci malesuada dapibus vel sit amet odio. Donec vel magna diam. Nulla porttitor posuere urna, nec hendrerit tortor. Cras vestibulum diam nibh. Curabitur facilisis urna sed dui bibendum, in mollis tellus varius. Vivamus feugiat sapien nec tellus finibus, eget maximus erat ornare. Morbi at cursus sapien.</p>
-                            </div>
-                            <a href="#">View more</a>
-                        </figcaption>           
-                    </figure>
-                    <figure class="effect-lily">
-                        <img src="https://tympanus.net/Development/HoverEffectIdeas/img/1.jpg" alt="img1"/>
-                        <figcaption>
-                            <div>
-                                <h2>Nice <span>Lily</span></h2>
-                                <p>Lily likes to play with crayons and pencils</p>
-                            </div>
-                            <a href="#">View more</a>
-                        </figcaption>           
-                    </figure>
-                </div>
-                
-            </div>
-    
-        
+
+    <div className="container_u">
+      <figure class="captionEffect" data-effect="fade-in">
+      <img src={Cubo}/>
+  <figcaption>
+    <h3>Cover Fade</h3>
+    <p>Lorem ipsum dolar.</p>
+  </figcaption>
+</figure>
+    </div>        
         
   )};
 
 export default History;
+
+{/* <img src={Cubo}/> */}
